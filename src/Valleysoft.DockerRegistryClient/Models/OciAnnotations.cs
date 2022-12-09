64265664c891f1b0cdb998fbe45225775747a4fb
Matt Thalman <mthalman@microsoft.com>
2022-12-08 18:52:45 -0600

    Constants for pre-defined OCI annotations

---
+++ src/Valleysoft.DockerRegistryClient/Models/OciAnnotations.cs
@@ -0,0 +1,25 @@
+﻿namespace Valleysoft.DockerRegistryClient.Models;
+
+/// <summary>
+/// Pre-defined annotation keys from the OCI spec.
+/// </summary>
+public static class OciAnnotations
+{
+    // https://github.com/opencontainers/image-spec/blob/v1.0/annotations.md
+
+    public const string Prefix = "org.opencontainers";
+    public const string ImagePrefix = $"{Prefix}.image";
+
+    public const string ImageCreated = $"{ImagePrefix}.created";
+    public const string ImageAuthors = $"{ImagePrefix}.authors";
+    public const string ImageUrl = $"{ImagePrefix}.url";
+    public const string ImageDocumentation = $"{ImagePrefix}.documentation";
+    public const string ImageSource = $"{ImagePrefix}.source";
+    public const string ImageVersion = $"{ImagePrefix}.version";
+    public const string ImageRevision = $"{ImagePrefix}.revision";
+    public const string ImageVendor = $"{ImagePrefix}.vendor";
+    public const string ImageLicenses = $"{ImagePrefix}.licenses";
+    public const string ImageRefName = $"{ImagePrefix}.ref.name";
+    public const string ImageTitle = $"{ImagePrefix}.title";
+    public const string ImageDescription = $"{ImagePrefix}.description";
+}
