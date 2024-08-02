22d9eec29d232c558199cad56ab4fcde0d01ed95
Matt Thalman <mthalman@microsoft.com>
2024-08-01 19:49:51 -0500

    Rename OciManifestList to OciImageIndex (#35)

--- src/Valleysoft.DockerRegistryClient/ManifestMediaTypes.cs
+++ src/Valleysoft.DockerRegistryClient/ManifestMediaTypes.cs
@@ -6,7 +6,7 @@ public static class ManifestMediaTypes
     public const string DockerManifestList = "application/vnd.docker.distribution.manifest.list.v2+json";
 
     public const string OciManifestSchema1 = "application/vnd.oci.image.manifest.v1+json";
-    public const string OciManifestList1 = "application/vnd.oci.image.index.v1+json";
+    public const string OciImageIndex1 = "application/vnd.oci.image.index.v1+json";
 
     //public const string DockerContainerConfig = "application/vnd.docker.container.image.v1+json";
     //public const string DockerGzippedTar = "application/vnd.docker.image.rootfs.diff.tar.gzip";
