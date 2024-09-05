010a45a633995e72e9082d19dba14a8894524d46
Matt Thalman <mthalman@microsoft.com>
2024-09-05 12:38:58 -0500

    Fix for Docker ManifestList to implement IManifestList

--- src/Valleysoft.DockerRegistryClient/Models/Manifests/Docker/ManifestList.cs
+++ src/Valleysoft.DockerRegistryClient/Models/Manifests/Docker/ManifestList.cs
@@ -5,7 +5,7 @@ namespace Valleysoft.DockerRegistryClient.Models.Manifests.Docker;
 /// <summary>
 /// The manifest list is the "fat manifest" which points to specific image manifests for one or more platforms. Its use is optional, and relatively few images will use one of these manifests. A client will distinguish a manifest list from an image manifest based on the Content-Type returned in the HTTP response.
 /// </summary>
-public class ManifestList : Manifest
+public class ManifestList : Manifest, IManifestList
 {
     public ManifestList()
     {
@@ -17,5 +17,7 @@ public class ManifestList : Manifest
     /// The manifests field contains a list of manifests for specific platforms.
     /// </summary>
     [JsonPropertyName("manifests")]
-    public ManifestReference[] Manifests { get; set; } = Array.Empty<ManifestReference>();
+    public ManifestReference[] Manifests { get; set; } = [];
+
+    IManifestReference[] IManifestList.Manifests => Manifests;
 }
